700cfd6a739e842e7204ff8585ee2628b2b52ec2
Santie Pelayo <spelayo@iripple.com>
2020-05-24 15:42:17 +0800

    updated add cors policy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
+var cors = require('cors')
 
 
-
-
-router.get('/customers/', async (req,res,next) => {
+router.get('/api/customers/',  cors(), async (req,res,next) => {
         try{
             let results = await db.all();
             res.json(results);
@@ -15,7 +14,7 @@ router.get('/customers/', async (req,res,next) => {
         }
 });
 
-router.get('/payments/', async (req,res,next) => {
+router.get('/api/payments/',  cors(), async (req,res,next) => {
     try{
         let results = await db.tblPayments();
         res.json(results);
@@ -25,7 +24,7 @@ router.get('/payments/', async (req,res,next) => {
     }
 });
 
-router.get('/customers/:customerId', async (req,res,next) => {
+router.get('/api/customers/:customerId',  cors(), async (req,res,next) => {
     try{
         let results = await db.one(req.params.customerId);
         res.json(results);
@@ -35,7 +34,7 @@ router.get('/customers/:customerId', async (req,res,next) => {
     }
 });
 
-router.delete('/customers/:customerId', async (req,res,next) => {
+router.delete('/api/customers/:customerId',  cors(), async (req,res,next) => {
     try{
         let results = await db.delete(req.params.customerId);
         //res.json(results);
@@ -46,7 +45,7 @@ router.delete('/customers/:customerId', async (req,res,next) => {
     }
 });
 
-router.post('/customers', async (req,res,next) => {
+router.post('/api/customers', cors(), async (req,res,next) => {
     try{
         let body = req.body
         let ifValid = await db.checkCustomerIfAlreadyExist(req.body.email);
@@ -68,7 +67,7 @@ router.post('/customers', async (req,res,next) => {
 });
 
 
-router.put('/customers/:customerId', async (req,res,next) => {
+router.put('/api/customers/:customerId',  cors(),async (req,res,next) => {
     try{
         let body = req.body
         let results = await db.update({"image": body.image, "meterNo": body.meterNo}, req.params.customerId);
@@ -80,7 +79,7 @@ router.put('/customers/:customerId', async (req,res,next) => {
     }
 });
 
-router.post('/login/staff',  async (req,res,next) => {
+router.post('/api/login/staff',  cors(), async (req,res,next) => {
 
     try{
         let body = req.body
@@ -98,7 +97,7 @@ router.post('/login/staff',  async (req,res,next) => {
     }
 });
 
-router.post('/login',  async (req,res,next) => {
+router.post('/api/login',   cors(),async (req,res,next) => {
 
     try{
         let body = req.body
@@ -120,7 +119,7 @@ router.post('/login',  async (req,res,next) => {
 
 });
 
-router.get('/generateCustomerId', async (req,res,next) => {
+router.get('/api/generateCustomerId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getLastRowCustomer();
         res.json(results[0]);
@@ -130,7 +129,7 @@ router.get('/generateCustomerId', async (req,res,next) => {
     }
 });
 
-router.get('/payments/:customerId', async (req,res,next) => {
+router.get('/api/payments/:customerId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getPaymentsCustomer([req.params.customerId]);
         res.json(results);
@@ -141,7 +140,7 @@ router.get('/payments/:customerId', async (req,res,next) => {
 });
 
 
-router.get('/invoices/:customerId', async (req,res,next) => {
+router.get('/api/invoices/:customerId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getInvoicesCustomer([req.params.customerId]);
         res.json(results);
@@ -152,7 +151,7 @@ router.get('/invoices/:customerId', async (req,res,next) => {
 });
 
 
-router.get('/invoices/payments/:invoiceId', async (req,res,next) => {
+router.get('/api/invoices/payments/:invoiceId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getDetailsOfPayment([req.params.invoiceId]);
         res.json(results);
@@ -163,7 +162,7 @@ router.get('/invoices/payments/:invoiceId', async (req,res,next) => {
 });
 
 
-router.get('/invoices/count/:customerId', async (req,res,next) => {
+router.get('/api/invoices/count/:customerId', cors(), async (req,res,next) => {
     try{
         let results = await db.getTotalInvoices([req.params.customerId]);
         res.json(results);
@@ -173,7 +172,7 @@ router.get('/invoices/count/:customerId', async (req,res,next) => {
     }
 });
 
-router.get('/invoices/done/:customerId', async (req,res,next) => {
+router.get('/api/invoices/done/:customerId', cors(), async (req,res,next) => {
     try{
         let results = await db.getCountDone([req.params.customerId]);
         res.json(results);
@@ -183,7 +182,7 @@ router.get('/invoices/done/:customerId', async (req,res,next) => {
     }
 });
 
-router.get('/invoices/pending/:customerId', async (req,res,next) => {
+router.get('/api/invoices/pending/:customerId', cors(), async (req,res,next) => {
     try{
         let results = await db.getCountPending([req.params.customerId]);
         res.json(results);
@@ -194,7 +193,7 @@ router.get('/invoices/pending/:customerId', async (req,res,next) => {
 });
 
 
-router.get('/invoices/reading/:customerId', async (req,res,next) => {
+router.get('/api/invoices/reading/:customerId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getLatestInvoice([req.params.customerId]);
         res.json(results);
@@ -204,7 +203,7 @@ router.get('/invoices/reading/:customerId', async (req,res,next) => {
     }
 });
 
-router.get('/invoices/beforeReading/:customerId', async (req,res,next) => {
+router.get('/api/invoices/beforeReading/:customerId',  cors(),async (req,res,next) => {
     try{
         let results = await db.getSecondInvoice([req.params.customerId]);
         res.json(results);
@@ -216,7 +215,7 @@ router.get('/invoices/beforeReading/:customerId', async (req,res,next) => {
 
 
 
-router.post('/invoices', async (req,res,next) => {
+router.post('/api/invoices', cors(), async (req,res,next) => {
     try{
         let body = req.body
         let results = await db.createInvoice([body.staffId,body.customerId,body.previousMeter,body.presentMeter, body.billingStart, body.billingEnd,body.dueDate,body.totalMeter,body.perCubicPrice,body.totalAmount,body.invoiceStatus,body.dateOfReading,body.remarks]);
@@ -233,7 +232,7 @@ router.post('/invoices', async (req,res,next) => {
 
 
 
-router.put('/invoices/:invoiceId', async (req,res,next) => {
+router.put('/api/invoices/:invoiceId', cors(), async (req,res,next) => {
     try{
         let body = req.body
         let results = await db.UpdateInvoice({"staffId": body.staffId, "presentMeter": body.presentMeter, "totalMeter": body.totalMeter, "dateOfReading": body.dateOfReading, "remarks": body.remarks, "totalAmount": body.totalAmount, "endOfReading": body.endOfReading }, req.params.invoiceId);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const apiRouter = require('./routes')
 const app = express();
+var http = require('http');
+var server = http.createServer(app)
+var cors = require('cors')
+
+var whitelist = ['http://52.77.224.13:3000', 'http://52.77.224.13:3000']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 // calling body-parser to handle the Request Object from POST requests
 var bodyParser = require('body-parser');
@@ -12,19 +26,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/api', apiRouter);
+app.use('/', apiRouter);
 
-app.use((req, res, next) => {
-     var err = new Error('Page not Found');
-     err.status = 404;
-     next(err);
-})
 
-app.use((err,req,next)=>{
-     res.status(err.status || 500);
-     res.send(err.message)
-})
 
-app.listen(process.env.PORT || '3000', () => {
-     console.log(`Server is running on port: ${process.env.PORT || '3000'}`);
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "localhost"); // update to match the domain you will make the request from
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+});
+
+// app.use((req, res, next) => {
+//      var err = new Error('Page not Found');
+//      err.status = 404;
+//      next(err);
+// })
+
+// app.use((err,req,next)=>{
+//      res.status(err.status || 500);
+//      res.send(err.message)
+// })
+
+server.listen(3000, 'localhost');
+server.on('listening', function() {
+    console.log('Express server started on port %s at %s', server.address().port, server.address().address);
 });
